ea2991ffd3d12463abc887505eb2c6f92740c2d4
CoderMandev <ongeresamuel6@gmail.com>
2024-05-06 20:43:37 +0300

    first commit

---
+++ prac.js
@@ -0,0 +1,16 @@
+/*var str="helloman"
+var chk = str[str.length -1]
+console.log(chk) */
+
+function madlib(x, y, z){
+    var output=""
+    output+="i am" + x + y + z
+     return output
+
+}
+console.log(madlib("kami", "sama", "dareda"))
+
+
+
+console.log(x)
+let x = 10
